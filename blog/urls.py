501a4d43a40c4a31b939997e17854fc3575c8c03
Ramesh Pradhan <ramesrest@gmail.com>
2020-07-09 00:15:02 +0545

    Separate templates into partals

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 
 from . import views
 
+app_name = 'blog'
+
 urlpatterns = [
-    path('', views.HomePage.as_view(), name='home')
+    path('', views.HomePage.as_view(), name='home'),
+    path('about/', views.AboutView.as_view(), name='about'),
+    path('blog/detail/', views.BlogDetailView.as_view(), name='blog_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -4,3 +4,11 @@ from django.views.generic import TemplateView, View
 
 class HomePage(TemplateView):
     template_name = 'blog/index.html'
+
+
+class BlogDetailView(TemplateView):
+    template_name = 'blog/blog_detail.html'
+
+
+class AboutView(TemplateView):
+    template_name = 'blog/about.html'
